bb894fc93bd219925b6e1917917b096179b71fb8
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-05-03 09:37:26 +0500

    update

--- lib/page1.dart
+++ lib/page1.dart
@@ -6,7 +6,17 @@ class Firstpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(title: Text("First page"), centerTitle: true),
+      body: Center(
+        child: Text(
+          "First page",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 100,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/page2.dart
+++ lib/page2.dart
@@ -6,7 +6,17 @@ class Secondpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text("Second page"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Second page",style: TextStyle(
+          color: Colors.black,
+          fontSize: 100,
+          fontWeight: FontWeight.bold
+        ),),
+      ),
     );
   }
 }
