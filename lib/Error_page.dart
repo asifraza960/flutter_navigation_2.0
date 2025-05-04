a373ca9b7e367a75f4f2d0ca33fc494727fb90ce
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-05-04 21:00:07 +0500

    update

--- lib/Error_page.dart
+++ lib/Error_page.dart
@@ -7,13 +7,21 @@ class ErrorPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          "404! page Not found",
-          style: TextStyle(
-            color: Colors.red,
-            fontWeight: FontWeight.bold,
-            fontSize: 100,
-          ),
+        child: Column(
+          children: [
+            Text(
+              "404! page Not found",
+              style: TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+                fontSize: 100,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Icon(Icons.exit_to_app_outlined),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/Nav_2_0/information_parsor.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+class MyinformationParsor extends RouteInformationParser<RouteSettings>{
+  
+}
